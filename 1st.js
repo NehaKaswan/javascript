fcbbf43fc52e1a430e7cae88fc68e0ae59bf053d
Neha Kaswan <nkaswan447@gmail.com>
2024-10-26 13:57:45 +0530

    d1

---
+++ 1st.js
@@ -0,0 +1,151 @@
+// // let Name = "Neha";
+// // // console .log(Name);
+// // if (Name === "Neha") alert("welcome");
+ 
+// // let age ="20"
+// // console.log(age);
+
+// // //datatypes
+// // let sneha =true;
+// // console.log(sneha);
+// // console.log(typeof sneha);
+
+// // let x;
+// // console.log("x");
+// // console.log(typeof x);
+
+// // const total = 1998 - 2005;
+// // const year = 2024 - 2004;
+// // console.log( total, year);
+ 
+// // const ageYear = 198 + 20000;
+// // console.log(ageYear); 
+  
+// // const multiple = 5000*290
+// // console.log ( multiple);
+
+// // const divide = 2790/3;
+// // console.log( divide);
+
+// // const remine =36%5;
+// // console.log (remine);
+
+// // //comparison operator
+ 
+// // let z = 20;
+// //  console.log(20 <=19);
+// //  console.log(20>=21);
+// //  console.log( 20>17);
+// //  console.log(20<21);
+
+// //operator precedence
+
+// const x= 1;
+// const y= 2;
+// const z= 4;
+// console.log(x-y);
+// console.log (x+y/2);
+// console.log(x+y-z*x+(x-z*y)/z);
+// console.log(x-y*(x*z));
+// console.log(z%y/y)
+
+// //string and template literals
+
+// const firstName = "Neha";
+// const lastName ="Bishnoi";
+// const job ="student";
+// const age ="2004";
+// const year ="2024";
+
+// const newLine= `I'm ${firstName} ${lastName}, a ${year-age}year old ${job}. `;
+// console.log(newLine);
+ 
+// console.log(`string
+//     and
+//     operator`);
+
+//if and else statement
+
+// const age =15;
+
+//  const newAge = age >= 18;
+//  if(newAge){
+//      console.log("able to vote.");
+// }
+// else{
+//     console.log("not able to vote.");
+// }
+
+//  let firstName ="Neha";
+//  console.log(typeof firstName);
+
+//  let y = null;
+//  console .log(typeof y);
+
+//type conversion
+
+// const firstName= "20";
+// const age = 21;
+// console.log(firstName+age);
+
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(typeof inputYear);
+// console.log(Number(inputYear) + 18);
+//    console.log(inputYear +18);
+// console.log(Number("Neha"));
+
+// console.log(String(23), 23);
+  
+ //type coercion
+
+ // console.log("22"+"2");
+// console.log("22"/2);
+ 
+// logical operator
+
+const  hasvote = true;
+const haslicence = true;
+ 
+console.log(hasvote && haslicence);
+console.log(hasvote || haslicence);
+console.log(!hasvote, !haslicence);
+
+if(hasvote && haslicence){
+   console.log("his age is +18");
+}
+else{
+   console.log("age is smaller than 18");
+}
+ //ternary operator
+ const age =18;
+ age >=18
+   ? console.log("able to vote")
+   : console.log("not able to vote");
+   
+//switch case
+
+const day ="monday";
+
+switch (day){
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thrusday":
+    console.log("Write Code Example");
+    break;
+  case "friday":
+    console.log("Record Video");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day");
+}
+
+
+// if(day=="monday")
+
---
+++ assign.js
@@ -0,0 +1,107 @@
+// /////////assignment------1////
+
+
+// let johnMass =92;
+// let johnHeight =1.95;
+// let johnBMI =johnMass/johnHeight **2;
+  
+
+// let markMass=78;
+// let markHeight =1.69;
+// let markBMI =markMass/markHeight **2;
+
+// let markHigherBMI =markBMI> johnBMI;
+// console.log(markHigherBMI);
+
+//  johnMass =85;
+//  johnHeight =1.76;
+//  johnBMI =johnMass/(johnHeight **2);
+  
+
+//  markMass=95;
+//  markHeight =1.88;
+//  markBMI =markMass/(markHeight **2);
+
+//  markHigherBMI =markBMI> johnBMI;
+//  console.log(markHigherBMI);
+    
+
+ 
+//  //////assignment-----2\\
+ 
+//   markHigherBMI = markBMI<johnBMI
+//  if (markHigherBMI) {
+//     console.log("Mark's BMI is higher than John's!");
+//  }
+//  else {
+//     console.log("John's BMI is higher than Mark's!");
+
+//  }
+
+
+
+//  ////assignment----3//
+ 
+// let dolphinScore =96;
+// dolphinScore=108;
+// dolphinScore=89;
+//  const averageDolphin= (96+108+89)/3;
+//  console.log(averageDolphin);
+
+// let koalasScore =88;
+// koalasScore =91;
+// koalasScore =110;
+//  const averageKoalas= (88+91+110)/3;
+//  console.log(averageKoalas);
+
+//  const winner= averageDolphin>averageKoalas;
+//  if (winner){
+//  console.log("dolphin is winner");
+//  }
+//  else{
+//     console.log("koalas is winner");
+//  }
+
+//  //assignment-----4//
+ 
+//  let bill = 20000;
+//     let x = 50 <= bill 
+//     x= bill <= 300;
+
+//  if(x){
+//     console.log("tip is 15%");
+//  }
+// else{
+//     console.log("tip is 20%");
+// } 
+ 
+// let billValue =275;
+// let tipValue=billValue*15/100;
+// // console.log(tipValue)
+// let finalValue =(billValue+tipValue);
+// console.log(finalValue);
+
+// billValue=40;
+// tipValue = billValue*20/100;
+//  finalValue=(billValue+tipValue);
+// console.log(finalValue);
+
+// billValue= 430;
+// tipValue =billValue*20/100;
+// finalValue=(billValue+tipValue);
+// console.log(finalValue);
+
+
+
+/////function///
+
+function add(x, y){
+   return 1+(x+y)/2
+}
+let a=2;
+let b=3;
+let c=4;
+
+console.log( add(a, b));
+console.log("average plus one of b and c is", add(b, c));
+console.log("average plus one of a and c is", add(a, c));
