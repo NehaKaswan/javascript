563a1bbbae6e43feb62b66ddeef23f345def3838
Neha Kaswan <nkaswan447@gmail.com>
2024-11-10 20:51:30 +0530

    g1

--- fundamental/game/script1.js
+++ fundamental/game/script1.js
@@ -1,25 +1,28 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'game world';
-// document.querySelector('.number').textContent = 20;
-document.querySelector('.score').textContent = 20;
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
+ document.querySelector('.message').textContent = 'game world';
+// document.querySelector('.number').textContent = 2;
+// document.querySelector('.score').textContent = 90;
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-// console.log(number);
-document.querySelector('.number').textContent = secretNumber;
+ let score = 20;
+//  console.log(number);
+ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
+    // if (score > 1) 
+    // score++;
+    // document.querySelector('.score').textContent = score;
+
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
